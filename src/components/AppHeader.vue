4ee85d20b19e01d4d273fe6dd9080a4e9fcd595d
DarioGiusino <dario.giusino@gmail.com>
2023-01-31 16:40:58 +0100

    styled header nav links w/ button inside

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,14 @@
 <script>
+import NavLinks from './NavLinks.vue';
+import BaseButton from './generics/BaseButton.vue';
 export default {
-    name: 'App Header'
+    name: 'App Header',
+    components: { NavLinks, BaseButton },
+    data() {
+        return {
+            headerLinks: ['Home', 'Courses', 'About Us', 'News', 'Pages', 'Contact', 'Purchase']
+        }
+    }
 }
 </script>
 
@@ -8,10 +16,15 @@ export default {
     <header>
         <!-- # upper navbar -->
         <nav class="d-flex justify-content-between align-items-center">
+            <!-- left logo -->
             <a href="#">
                 <img class="header-logo" src="../assets/img/theme_eduprime_logo.png" alt="edu prime logo">
             </a>
 
+            <!-- right navbar -->
+            <nav-links class="links" :links="headerLinks">
+                <base-button class="nav-button" button-label="view courses"></base-button>
+            </nav-links>
         </nav>
 
         <!-- # jumbotron -->
@@ -26,6 +39,7 @@ header {
     background-image: url('../assets/img/theme_slider2_bg-1.jpg');
     background-repeat: no-repeat;
     background-size: cover;
+    color: $text-white-1;
 
     nav {
         padding: 1.75rem;
@@ -33,6 +47,18 @@ header {
 
         .header-logo {
             width: 16rem;
+        }
+
+        .links {
+            font-weight: 500;
+
+            .nav-button {
+                background-color: $bg-yellow;
+                color: $text-white-1;
+                text-transform: uppercase;
+                font-size: 13px;
+                font-weight: 500;
+            }
         }
     }
 }
---
+++ src/components/NavLinks.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+    name: 'Navbar',
+    props: { links: Array }
+}
+</script>
+
+<template>
+    <ul>
+        <li v-for="link, i in links" :key="i"><a href="#">{{ link }}</a></li>
+        <!-- slot for eventually a button -->
+        <slot></slot>
+    </ul>
+</template>
+
+<style scoped lang="scss">
+@use '../assets/scss/partials/variables.scss' as *;
+
+ul {
+    margin-bottom: 0;
+    list-style-type: none;
+    display: flex;
+    align-items: center;
+    gap: 20px;
+
+    li:first-child {
+        color: $text-yellow;
+    }
+
+    li:nth-child(1):after,
+    li:nth-child(2):after,
+    li:nth-child(4):after,
+    li:nth-child(5):after {
+        content: '\25BC';
+        font-size: 0.5rem;
+        margin-left: 0.3rem;
+        vertical-align: middle;
+    }
+}
+</style>
