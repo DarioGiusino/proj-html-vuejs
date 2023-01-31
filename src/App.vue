1b618bf8ac41eba7d6f479fb5222aae0dadd8bd7
DarioGiusino <dario.giusino@gmail.com>
2023-01-31 19:10:35 +0100

    created and linked app main component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
 export default {
   name: 'Edu Prime',
-  components: { AppHeader }
+  components: { AppHeader, AppMain }
 }
 </script>
 
 <template>
   <app-header></app-header>
-
+  <app-main></app-main>
 </template>
 
 <style lang="scss">
