80917ec20a506768309df84f17477ab424acb958
DarioGiusino <dario.giusino@gmail.com>
2023-01-31 17:16:09 +0100

    added fontawesome + 2 icons

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import { createApp } from "vue";
+// bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
+
+//fontawesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  faMagnifyingGlass,
+  faUserPlus,
+} from "@fortawesome/free-solid-svg-icons";
+library.add(faMagnifyingGlass, faUserPlus);
+
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
