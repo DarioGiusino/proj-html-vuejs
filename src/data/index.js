91e25aafa4514b60f7c37c08ce0f2fe4b59c98a0
DarioGiusino <dario.giusino@gmail.com>
2023-02-01 10:25:16 +0100

    created faculties and courses in index

---
+++ src/data/index.js
@@ -0,0 +1,51 @@
+const faculties = [
+  {
+    name: "Law Faculty",
+    image: "Gavel",
+  },
+  {
+    name: "Economy",
+    image: "Coin",
+  },
+  {
+    name: "Medicine",
+    image: "Medicine",
+  },
+  {
+    name: "Computer Science",
+    image: "Computer",
+  },
+  {
+    name: "Graphic Design",
+    image: "Palette",
+  },
+];
+
+const courses = [
+  {
+    title: "Make better Decisions",
+    teacher: "James Collins",
+    price: "$21.00",
+    image: "Decisions",
+  },
+  {
+    title: "How to be a speaker",
+    teacher: "James Collins",
+    price: "Free",
+    image: "Speaker",
+  },
+  {
+    title: "Network introductions",
+    teacher: "James Collins",
+    price: "Free",
+    image: "Network",
+  },
+  {
+    title: "Brand Management",
+    teacher: "James Collins",
+    price: "Free",
+    image: "Brand",
+  },
+];
+
+export { faculties, courses };
