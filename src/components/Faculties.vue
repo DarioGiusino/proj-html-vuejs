ce502fa93007a04904f2e740af4fe92f46e07025
DarioGiusino <dario.giusino@gmail.com>
2023-02-01 10:17:58 +0100

    line structure +change name to img

--- src/components/Faculties.vue
+++ src/components/Faculties.vue
@@ -5,10 +5,22 @@ export default {
 </script>
 
 <template>
+    <!-- # title & desc header -->
     <header class="text-center">
         <h1 class="mb-3">Faculties available at EduPrime</h1>
         <p class="mb-3">A single universisty with a load of courses tailored to satisfy any student's needs.</p>
     </header>
+
+    <!-- # cards -->
+    <main>
+        <div class="line">
+            <div class="container">
+                <div class="row row-cols-5">
+                    <div class="col"></div>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
 
 <style acoped lang="scss">
