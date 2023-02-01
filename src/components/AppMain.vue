09d49f2200f8df245c7c270763dc4a741e63eb88
DarioGiusino <dario.giusino@gmail.com>
2023-02-01 10:04:43 +0100

    background to presentation section

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -8,21 +8,24 @@ export default {
 </script>
 
 <template>
-    <!-- # faculties section -->
-    <section id="faculties">
-        <!-- ? upper faculties section -->
+    <!-- # many courses section -->
+    <section id="presentation">
+        <!-- ? presentation section -->
         <div class="container">
             <div class="row">
                 <div class="col-8">
+                    <!-- grey icon -->
                     <figure class="section-icon">
                         <img src="../assets/img/Books-icon.png" alt="books">
                     </figure>
+                    <!-- description -->
                     <base-section-desc title="Thousands of courses for any type of student"
                         paragraph="At EduPrime, it doesn't matter what domain you wish to pursue a career in. Here you can find a course that satisfies your needs within a click away and applies for a course in a matter of minutes. EduPrime is ranked as the most versatile university in the world, thanks to the number of courses it provides.">
                         <base-button class="yellow-btn" button-label="Browse through courses"></base-button>
                     </base-section-desc>
                 </div>
                 <div class="col-4">
+                    <!-- big image -->
                     <img src="../assets/img/Graduation-Illustration.png" alt="graduation">
                 </div>
             </div>
@@ -31,5 +34,11 @@ export default {
 </template>
 
 <style scoped lang="scss">
-
+#presentation {
+    padding: 2.5rem 0;
+    background-image: url('../assets/img/home-background.png');
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: right;
+}
 </style>
