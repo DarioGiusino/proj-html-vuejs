9f11087861b8c6dd26515bbff489ebd731635c4c
DarioGiusino <dario.giusino@gmail.com>
2023-02-01 13:37:14 +0100

    isolated presentation row in component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,34 +2,20 @@
 import BaseSectionDesc from './generics/BaseSectionDesc.vue';
 import BaseButton from './generics/BaseButton.vue';
 import Faculties from './main/Faculties.vue';
+import PresentationRow from './main/PresentationRow.vue';
 export default {
     name: 'App Main',
-    components: { BaseSectionDesc, BaseButton, Faculties }
+    components: { BaseSectionDesc, BaseButton, Faculties, PresentationRow }
 }
 </script>
 
 <template>
     <!-- # courses presentation section -->
     <section id="courses-presentation">
-        <div class="container">
-            <div class="row">
-                <div class="col-8">
-                    <!-- grey icon -->
-                    <figure class="section-icon">
-                        <img src="../assets/img/Books-icon.png" alt="books">
-                    </figure>
-                    <!-- description -->
-                    <base-section-desc title="Thousands of courses for any type of student"
-                        paragraph="At EduPrime, it doesn't matter what domain you wish to pursue a career in. Here you can find a course that satisfies your needs within a click away and applies for a course in a matter of minutes. EduPrime is ranked as the most versatile university in the world, thanks to the number of courses it provides.">
-                        <base-button class="yellow-btn" button-label="Browse through courses"></base-button>
-                    </base-section-desc>
-                </div>
-                <div class="col-4">
-                    <!-- big image -->
-                    <img src="../assets/img/Graduation-Illustration.png" alt="graduation">
-                </div>
-            </div>
-        </div>
+        <presentation-row icon="Books-icon.png" image="Graduation-Illustration.png"
+            title="Thousands of courses for any type of student"
+            paragraph="At EduPrime, it doesn't matter what domain you wish to pursue a career in. Here you can find a course that satisfies your needs within a click away and applies for a course in a matter of minutes. EduPrime is ranked as the most versatile university in the world, thanks to the number of courses it provides."
+            button-color="yellow-btn" button-label="Browse through courses"></presentation-row>
     </section>
 
     <!-- # faculties -->
---
+++ src/components/main/PresentationRow.vue
@@ -0,0 +1,52 @@
+<script>
+import BaseButton from '../generics/BaseButton.vue';
+import BaseSectionDesc from '../generics/BaseSectionDesc.vue';
+export default {
+    name: 'Presentation Row',
+    components: { BaseButton, BaseSectionDesc },
+    props: {
+        icon: String,
+        image: String,
+        title: String,
+        paragraph: String,
+        buttonColor: String,
+        buttonLabel: String,
+        direction: String
+    },
+    methods: {
+        // build image 
+        buildImage(path) {
+            //build src
+            const url = new URL(`../../assets/img/${path}`, import.meta.url);
+
+            //return src
+            return url.href;
+        },
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="row" :class="direction">
+            <div class="col-8">
+                <!-- grey icon -->
+                <figure v-if="icon" class="section-icon">
+                    <img :src="buildImage(icon)" :alt="icon">
+                </figure>
+                <!-- description -->
+                <base-section-desc :title="title" :paragraph="paragraph">
+                    <base-button :class="buttonColor" :button-label="buttonLabel"></base-button>
+                </base-section-desc>
+            </div>
+            <div class="col-4">
+                <!-- big image -->
+                <img :src="buildImage(image)" :alt="image">
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
