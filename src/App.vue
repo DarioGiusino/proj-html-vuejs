9107bf1844e73fc66ceb7b9aae47823c0b02c3e9
DarioGiusino <dario.giusino@gmail.com>
2023-02-02 09:37:34 +0100

    created app footer comp

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
 export default {
   name: 'Edu Prime',
-  components: { AppHeader, AppMain }
+  components: { AppHeader, AppMain, AppFooter }
 }
 </script>
 
 <template>
   <app-header></app-header>
   <app-main></app-main>
+  <app-footer></app-footer>
 </template>
 
 <style lang="scss">
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'App Footer'
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoper lang="scss">
+
+</style>
