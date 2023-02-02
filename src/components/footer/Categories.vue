4f130bba5ab1d5baf325ce9c03eefcdfe2159fcf
DarioGiusino <dario.giusino@gmail.com>
2023-02-02 11:20:23 +0100

    save befor palette change

--- src/components/footer/Categories.vue
+++ src/components/footer/Categories.vue
@@ -1,13 +1,20 @@
 <script>
+import BaseInputText from '../generics/BaseInputText.vue';
 export default {
-    name: 'Footer Categories'
+    name: 'Footer Categories',
+    components: { BaseInputText }
 }
 </script>
 
 <template>
+    <!-- input text -->
+    <base-input-text placeholder="Search..."><font-awesome-icon class="me-1 prova"
+            icon="fa-solid fa-magnifying-glass" /></base-input-text>
 
 </template>
 
 <style scoped lang="scss">
-
+.prova {
+    position: relative;
+}
 </style>
