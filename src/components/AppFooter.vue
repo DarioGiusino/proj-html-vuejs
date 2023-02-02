0397916a668b2b276e9161dce4727afb99eee3bf
DarioGiusino <dario.giusino@gmail.com>
2023-02-02 10:00:12 +0100

    preworked social links comp

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,13 +1,37 @@
 <script>
+import SocialLinks from './footer/SocialLinks.vue';
 export default {
-    name: 'App Footer'
+    name: 'App Footer',
+    components: { SocialLinks }
 }
 </script>
 
 <template>
+    <!-- # footer -->
+    <footer>
+        <div class="container">
+            <div class="row row-cols-3">
+                <!-- ? social links (right footer) -->
+                <div class="col">
+                    <social-links></social-links>
+                </div>
 
+                <!-- ? nav lists (central footer) -->
+                <div class="col"></div>
+
+                <!-- ? categories (left footer) -->
+                <div class="col"></div>
+            </div>
+        </div>
+    </footer>
 </template>
 
-<style scoper lang="scss">
+<style scoped lang="scss">
+@use '../assets/scss/partials/variables.scss' as *;
 
+footer {
+    background-color: $bg-red-2;
+    color: $text-white-2;
+    padding: 6rem 0;
+}
 </style>
---
+++ src/components/footer/SocialLinks.vue
@@ -0,0 +1,40 @@
+<script>
+import BaseButton from '../generics/BaseButton.vue';
+export default {
+    name: 'Footer Social Links',
+    components: { BaseButton }
+}
+</script>
+
+<template>
+    <!-- upper logo -->
+    <header>
+        <img class="w-50" src="../../assets/img/theme_eduprime_logo.png" alt="edu logo">
+    </header>
+
+    <!-- central description -->
+    <main>
+        <p class="mb-0">
+            EduPrime is the most versatile WordPress theme for educational purposes, showcasing universities, courses,
+            secondary schools etc.
+        </p>
+    </main>
+
+    <!-- social links -->
+    <footer>
+        <base-button class="social-btn"></base-button>
+    </footer>
+</template>
+
+<style scoped lang="scss">
+@use '../../assets/scss/partials/variables.scss' as *;
+
+.social-btn {
+    background-color: transparent;
+    border-color: $border-yellow-1;
+    padding: 5px 5px;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+}
+</style>
